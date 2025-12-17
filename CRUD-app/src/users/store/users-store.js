c0ea82ef8f0f0246f05f467970df8767e86c538b
Franco Blanchard <blanchardfranco@gmail.com>
2025-12-17 07:10:49 -0300

    agrego validacion y actualizo state

--- CRUD-app/src/users/store/users-store.js
+++ CRUD-app/src/users/store/users-store.js
@@ -8,6 +8,13 @@ const state = {
 
 const loadNextPage= async () =>{
     await loadUsersByPage(state.currentPage + 1);   
+
+    //si no hay usuarios 
+    if(users.length === 0) returns;
+    
+    //si hay usuarios incrementa current y duarda los users de esa pagina en state
+    state.currentPage += 1;
+    state.users = users;
 }
 
 const loadPreviousPage= async () =>{
@@ -28,7 +35,7 @@ export default{
     onUserChanged,
     reloadPage,
     
-    getUser: () => [...state.users],
+    getUsers: () => [...state.users],
     getCurrentPage: () => state.currentPage,
 }
 
